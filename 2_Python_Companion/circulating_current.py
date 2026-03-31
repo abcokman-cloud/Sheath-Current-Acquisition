1eaee2cde3c1de75ed14bd15f842d0851fd6dfe5
User <user@example.com>
2026-03-31 10:14:02 +0800

    fix: adjust Dash config for Cloud deployment

--- 2_Python_Companion/circulating_current.py
+++ 2_Python_Companion/circulating_current.py
@@ -4,7 +4,7 @@ from current_callbacks import register_callbacks
 from current_views import create_layout
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(__name__, suppress_callback_exceptions=True, serve_locally=True)
 app.index_string = '''
 <!DOCTYPE html>
 <html>
@@ -46,4 +46,4 @@ register_callbacks(app)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host="127.0.0.1", port=8050)
+    app.run(debug=False, host="0.0.0.0", port=8050)
